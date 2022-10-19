b56f4f9bff998cdac6579b64e78fd036f31eae3f
Agus Siswanto <cak_guess@ymail.com>
2022-10-20 01:25:42 +0700

    complete the comment component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -52,7 +52,12 @@ export default function PostPage({ newsResult, userRandomResults }) {
                {comments.length > 0 && (
                   <div className=''>
                      {comments.map((comment) => (
-                        <Comment key={comment.id} id={comment.id} comment={comment.data()} />
+                        <Comment
+                           key={comment.id}
+                           commentId={comment.id}
+                           originalPostId={id}
+                           comment={comment.data()}
+                        />
                      ))}
                   </div>
                )}
